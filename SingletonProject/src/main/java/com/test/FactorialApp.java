7e5f9d7835b327609b0082c13c94a22fe7b3f9d6
Janardhan Gollamudi <C5056743@cammis.local>
2016-08-10 11:13:59 -0700

    Commit#1

---
+++ SingletonProject/src/main/java/com/test/FactorialApp.java
@@ -0,0 +1,18 @@
+package com.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class FactorialApp 
+{
+   public int getFactorial(int value){
+	   if(value==0)
+		   return 1;
+	   int factorial = 1;
+	   for(int i=1;i<=value;i++){
+		   factorial = factorial * i;
+	   }
+	   return factorial;
+   }
+}
