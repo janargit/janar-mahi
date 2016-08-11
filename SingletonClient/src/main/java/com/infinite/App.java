e6a09183e2a7a9a18d1b7d6a9f431e141ab6d312
Janardhan Gollamudi <C5056743@cammis.local>
2016-08-11 16:40:34 -0700

    Pushing

--- SingletonClient/src/main/java/com/infinite/App.java
+++ SingletonClient/src/main/java/com/infinite/App.java
@@ -1,5 +1,7 @@
 package com.infinite;
 
+import com.test.FactorialApp;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        FactorialApp app = new FactorialApp();
+        System.out.println("Factorial of 10::"+app.getFactorial(10));
     }
 }
